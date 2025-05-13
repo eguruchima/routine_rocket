c65c8d2616d0e9d3279b13b6d64514f8446e66a9
eguruchima <eguruchima@gmail.com>
2025-05-13 14:41:16 -0400

    added tests and created crud for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render :index
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render :show
+  end
+
+  def create
+    @task = Task.create!(task_params)
+    render :show
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    render :show
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def task_params
+    params.permit(:title, :due_at, :category, :auto_remind, :user_id)
+  end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
-class TaskControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  test "index returns all tasks" do
+    get tasks_url(format: :json)
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal Task.count, body.length
+  end
+
+  test "show returns a single task" do
+    task = Task.first
+    get task_url(task, format: :json)
+    assert_response :success
+    data = JSON.parse(response.body)
+    assert_equal task.title, data["title"]
+  end
 end

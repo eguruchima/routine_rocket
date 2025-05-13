a0c15f74001fba3fa43b9add85c8d02264790fc9
eguruchima <eguruchima@gmail.com>
2025-05-13 14:50:39 -0400

    finished tests for tasks with green lights on all tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -15,4 +15,36 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal task.title, data["title"]
   end
+
+  test "create adds a task" do
+    user = users(:one)
+    assert_difference "Task.count", 1 do
+      post tasks_url(format: :json), params: {
+        title: "New Task",
+        due_at: 1.day.from_now,
+        category: "testing",
+        auto_remind: true,
+        user_id: user.id
+      }
+    end
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal "New Task", body["title"]
+  end
+
+  test "update edits a task" do
+    task = tasks(:one)
+    patch task_url(task, format: :json), params: { title: "Updated" }
+    assert_response :success
+    body = JSON.parse(response.body)
+    assert_equal "Updated", body["title"]
+  end
+
+  test "destroy removes a task" do
+    task = tasks(:one)
+    assert_difference "Task.count", -1 do
+      delete task_url(task, format: :json)
+    end
+    assert_response :no_content
+  end
 end
